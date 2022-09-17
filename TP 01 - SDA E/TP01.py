a42730b5b0c8aee4e06df8473fb47a46bf6ee1bb
eugeniusms <eugeniusms@gmail.com>
2022-09-17 11:29:58 +0700

    Add Some Files

---
+++ TP 01 - SDA E/TP01.py
@@ -0,0 +1,7 @@
+penyihir = ["Tara", "Kenneth"]
+
+penyihir_duluan = input()
+cawan_1 = int(input())
+cawan_2 = int(input())
+
+
---
+++ ide_max.py
@@ -0,0 +1,9 @@
+maks = 0
+
+for i in range(5):
+    variabel = int(input())
+
+    if variabel > maks: # 6 > 4 ? True
+        ...
+    else:
+        ...
