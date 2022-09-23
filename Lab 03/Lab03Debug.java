aa03b39bdb6c8103f43a5920cfdd9f3205faf8b2
eugeniusms <eugeniusms@gmail.com>
2022-09-23 13:53:45 +0700

    Backup Lab03 to Lab03 Debug

--- Lab 03/Lab03Debug.java
+++ Lab 03/Lab03Debug.java
@@ -24,52 +24,49 @@ public class Lab03Debug {
         }
 
         // Run Solusi
-        int solution = getMaxRedVotes(0, N);
+        int solution = getMaxRedVotes(0, N, 0);
         out.print(solution);
 
         // Tutup OutputStream
         out.close();
     }
 
-    public static int getMaxRedVotes(int start, int end) {
-        out.println("start: " + start + " | end: " + end); // TEST
+    public static int getMaxRedVotes(int start, int end, int redSave) {
         // Jika start == end maka sudahi rekursi
         if (start == end) {
             return 0;
         }
 
-        // Inisiasi variabel
-        int maxVotes = 0;
         int red = 0;
         int blue = 0;
         int redVotes = 0;
-        // Untuk setiap pecahan nilai iterasi from start -> i sampai end
+        int redSaveNew = 0; // redSaveNew digunakan untuk menyimpan nilai total vote red terbaru
         for (int i = start; i < end; i++) {
-            // Menghitung jumlah R, B pada setiap iterasi panjang A
             if (A[i] == 'R') {
-                red += 1;
+                red++;
             } else {
-                blue += 1;
+                blue++;
             }
 
-            // Mengambil data redVotes 
+            // Mendapatkan redvotes di setiap perulangan dalam satu kolom
             redVotes = getVotes(red, blue);
-            // Mencatat nilai max dari vote yang ada
-            if (redVotes > maxVotes) {
-                maxVotes = redVotes;
-                // Mencatat nilai start letak perpotongan array pada nilai maxnya
-                start = i;
-            }
+            out.println("redVotes: " + redVotes);
+
+            // Menyimpan nilai total redVote ditambah redSave sebelumnya
+            // Example: (R R B)(B) => redSave = 3 (redVote sebelum saat ini) + 0 (redVote saat ini)
+            redSaveNew = redSave + redVotes; 
+
+            // Pecah lagi pada setiap perulangan menuju baris sequence masing-masing (sisa sequence)
+            // Berdasarkan data setelah start terakhir = i + 1 sampai end
+            getMaxRedVotes(i+1, end, redSaveNew);
         }
 
-        out.println("max: " + maxVotes); // TEST
+        out.println("======================");
+        // redSaveNew adalah jumlah total vote red dalam satu subsequence 
+        out.println("redsavenew: " + redSaveNew);
+        out.println();
 
-        // Terdapat 2 kasus lanjutan:
-        // 1) Semua nilai berisikan 0, artinya start = posisi terdepan potongan array
-        // 2) Ada satu nilai max, artinya start = posisi nilai maxnya
-        // Namun keduanya memiliki pendekatan next yang sama
-        // Sama-sama next pada start + 1 (sisa potongan array)
-        return maxVotes + getMaxRedVotes(start + 1, end);
+        return redSaveNew;
     }
 
     // Fungsi digunakan untuk mengembalikan jumlah vote berdasarkan (red, blue) 
