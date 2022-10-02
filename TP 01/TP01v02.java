9bf23526fee14528c0b315ea0331e5ed8c84af0c
eugeniusms <eugeniusms@gmail.com>
2022-10-02 18:49:13 +0700

    Add v02 of TP01

---
+++ TP 01/TP01v02.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class TP01v02 {
+    private static InputReader in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) {
+        
+
+    }
+
+    // taken from https://codeforces.com/submissions/Petr
+    // together with PrintWriter, these input-output (IO) is much faster than the
+    // usual Scanner(System.in) and System.out
+    // please use these classes to avoid your fast algorithm gets Time Limit
+    // Exceeded caused by slow input-output (IO)
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public char nextChar() {
+            return next().charAt(0);
+        }
+    }
+}
