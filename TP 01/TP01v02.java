4cf1239fdb5d0cbda63a5b90ed280d4b68d096d1
eugeniusms <eugeniusms@gmail.com>
2022-10-02 20:37:36 +0700

    Add Pelanggan Input Harian

--- TP 01/TP01v02.java
+++ TP 01/TP01v02.java
@@ -6,8 +6,17 @@ public class TP01v02 {
     private static InputReader in;
     private static PrintWriter out;
 
+    // Rules:
+    // *) Indexing data dalam array dimulai dari 1 (bukan 0)
+    // *) Isi default sebuah array of object, data field int = 0, char = 'O'
+
+    // default arr[0] = kosong
     public static Makanan[] menu; // index => id makanan
     public static Koki[] koki; // index => id koki
+    public static Pelanggan[] pelanggan; // index => id pelanggan
+
+    // get status by queue
+    public static int[] KbyQueue = new int[100069];
 
     public static void main(String[] args) {
         InputStream inputStream = System.in;
@@ -15,29 +24,91 @@ public class TP01v02 {
         OutputStream outputStream = System.out;
         out = new PrintWriter(outputStream);
 
+        // ---------------------------- AMBIL INPUT DEFAULT -------------------------------------
         // ambil jumlah menu makanan
         int jumlahMenu = in.nextInt();
-        menu = new Makanan[jumlahMenu];
+        menu = new Makanan[jumlahMenu+1]; menu[0] = new Makanan(0, 'O'); // set default
         // membaca input menu makanan
-        for (int i = 0; i < jumlahMenu; i++) {
+        for (int i = 1; i <=jumlahMenu; i++) {
             menu[i] = new Makanan(in.nextInt(), in.nextChar());
         }
 
         // ambil jumlah koki
         int jumlahKoki = in.nextInt();
-        koki = new Koki[jumlahKoki];
+        koki = new Koki[jumlahKoki+1]; koki[0] = new Koki('O'); // set default
         // membaca input koki
-        for (int i = 0; i < jumlahKoki; i++) {
+        for (int i = 1; i <= jumlahKoki; i++) {
             koki[i] = new Koki(in.nextChar());
         }
 
-        check();
+        // ambil jumlah pelanggan
+        int jumlahPelanggan = in.nextInt(); 
+        pelanggan = new Pelanggan[jumlahPelanggan+1]; pelanggan[0] = new Pelanggan();
+        // generate pelanggan default
+        for (int i = 1; i <= jumlahPelanggan; i++) {
+            pelanggan[i] = new Pelanggan();
+        }
+
+        // ambil jumlah kursi dan jumlah hari
+        int jumlahKursi = in.nextInt();
+        int jumlahHari = in.nextInt();
+
+        check1();
+
+        for (int i = 1; i <= jumlahHari; i++) {
+            jalaniHari();
+            check2(i);
+        }
 
         // Tutup OutputStream
         out.close();
     }
 
-    public static void check() {
+    public static void jalaniHari() {
+        // ---------------------------- AMBIL INPUT HARIAN -------------------------------------
+        int jumlahPelangganHarian = in.nextInt();
+        // membaca input pelanggan harian
+        for (int i = 1; i <= jumlahPelangganHarian; i++) {
+
+            // ambil i, k, u, r (opsional)
+            int id = in.nextInt();
+            char k = in.nextChar(); 
+            int u = in.nextInt();  pelanggan[id].setU(u);
+
+            // memasukkan pelanggan sesuai idnya
+            if (k == '?') {
+                int r = in.nextInt(); 
+                pelanggan[id].setK(getKFromQueue(i, r));
+            } else {
+                int ket =  (k == '+' ? 1 : -1);  // jika + => 1, jika - => -1
+                KbyQueue[i] = ket;
+                pelanggan[id].setK(ket);
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            out.println("CEK: "+KbyQueue[i]);
+        }
+    }
+
+    // Fungsi untuk memberi status pada pelanggan bertipe = "?"
+    public static int getKFromQueue(int id, int jarak) {
+        int sumStatus = 0;
+        int indeks = id;
+        while (jarak > 0) {
+            indeks--; jarak--; 
+            sumStatus += KbyQueue[indeks];
+            // out.println("CEK: "+ indeks + " | " + jarak + " | " + sumStatus); // TEST
+        }
+        // Memberi status pada pelanggan
+        if (sumStatus > 0) {
+            KbyQueue[id] = 1; return 1; // K = positif
+        } else {
+            KbyQueue[id] = -1; return -1; // K = Negatif
+        }
+    }
+
+    public static void check1() {
         int counter = 0;
         // cek makanan
         out.println("-------------------------------------");
@@ -53,6 +124,20 @@ public class TP01v02 {
         for (Koki k: koki) {
             counter++;
             out.println(counter + ") " + k.getTipe() + " | " + k.getJumlahPelayanan());
+        }
+    }
+
+    // cek harian
+    public static void check2(int hari) {
+        int counter = 0;
+        out.println("-------------------------------------");
+        out.println("HARI KE-" + hari);
+        // cek pelanggan
+        out.println("-------------------------------------");
+        out.println("CEK PELANGGAN: [K] [U] [blacklist]");
+        for (Pelanggan p: pelanggan) {
+            counter++;
+            out.println(counter + ") " + p.getK() + " | " + p.getU() + " | " + p.isBlacklist());
         }
     }
 
@@ -118,7 +203,7 @@ class Koki {
     private char tipe;
     private int jumlahPelayanan;
 
-    // construct makanan
+    // construct koki
     Koki(char tipe) {
         this.tipe = tipe;
         this.jumlahPelayanan = 0; // default = 0
@@ -137,5 +222,49 @@ class Koki {
     // setter jumlahPelayanan
     public void setJumlahPelayanan(int jumlahPelayanan) {
         this.jumlahPelayanan = jumlahPelayanan;
+    }
+}
+
+// class inisiator pelanggan
+class Pelanggan {
+    private int K; // K > 0 (positif), K <= 0 (negatif)
+    private int U;
+    private boolean blacklist;
+
+    // construct pelanggan default
+    Pelanggan() {
+        this.K = 0; // default 0
+        this.U = 0; // default 0
+        this.blacklist = false; // default = false
+    }
+   
+    // getter K : Keterangan
+    public int getK() {
+        return this.K;
+    }
+
+    // getter U : Uang
+    public int getU() {
+        return this.U;
+    }
+
+    // getter blacklist
+    public boolean isBlacklist() {
+        return this.blacklist;
+    }
+
+    // setter K : Keterangan
+    public void setK(int K) {
+        this.K = K;
+    }
+
+    // setter U : Uang
+    public void setU(int U) {
+        this.U = U;
+    }
+
+    // setter blacklist => mengubah pelanggan jadi blacklisted
+    public void setBlacklist() {
+        this.blacklist = true;
     }
 }
