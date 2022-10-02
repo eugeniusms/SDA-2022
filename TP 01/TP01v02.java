56bbe3c21e9495c6c6a55f6adaaa65240750e581
eugeniusms <eugeniusms@gmail.com>
2022-10-02 19:07:11 +0700

    Add Menu Makanan

--- TP 01/TP01v02.java
+++ TP 01/TP01v02.java
@@ -6,9 +6,35 @@ public class TP01v02 {
     private static InputReader in;
     private static PrintWriter out;
 
-    public static void main(String[] args) {
-        
+    public static Makanan[] menu;
 
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        in = new InputReader(inputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
+
+        // ambil jumlah menu makanan
+        int jumlahMenu = in.nextInt();
+        menu = new Makanan[jumlahMenu];
+
+        // membaca input menu makanan
+        for (int i = 0; i < jumlahMenu; i++) {
+            menu[i] = new Makanan(in.nextInt(), in.nextChar());
+        }
+
+        check();
+
+        // Tutup OutputStream
+        out.close();
+    }
+
+    public static void check() {
+        // cek makanan
+        out.println("CEK MENU: ");
+        for (Makanan m: menu) {
+            out.println(m.getHarga() + " " + m.getTipe());
+        }
     }
 
     // taken from https://codeforces.com/submissions/Petr
@@ -43,5 +69,27 @@ public class TP01v02 {
         public char nextChar() {
             return next().charAt(0);
         }
+    }
+}
+
+// class inisiator makanan
+class Makanan {
+    private int harga;
+    private char tipe;
+
+    // construct makanan
+    Makanan(int harga, char tipe) {
+        this.harga = harga;
+        this.tipe = tipe;
+    }
+   
+    // getter harga
+    public int getHarga() {
+        return this.harga;
+    }
+
+    // getter tipe
+    public char getTipe() {
+        return this.tipe;
     }
 }
