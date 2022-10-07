60c318c913f8dd99c9864f65e118d0195e537334
eugeniusms <eugeniusms@gmail.com>
2022-10-07 08:19:14 +0700

    Start Lab04 Inisiasi Object

---
+++ Lab 04/Lab04.java
@@ -0,0 +1,133 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Lab04 {
+
+    private static InputReader in;
+    static PrintWriter out;
+
+    // [(gedung A),(gedung B),...]
+    public static Gedung[] kompleks;
+    // ex: (gedung A)
+    // first: lantai 1, .next: lantai 2 dst
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        in = new InputReader(inputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
+
+        int jumlahGedung = in.nextInt();
+        for (int i = 0; i < jumlahGedung; i++) {
+            String namaGedung = in.next();
+            int jumlahLantai = in.nextInt();
+
+            // TODO: Inisiasi gedung pada kondisi awal
+
+        }
+
+        String gedungDenji = in.next();
+        int lantaiDenji = in.nextInt();
+        // TODO: Tetapkan kondisi awal Denji
+
+        String gedungIblis = in.next();
+        int lantaiIblis = in.nextInt();
+        // TODO: Tetapkan kondisi awal Iblis
+
+        int Q = in.nextInt();
+
+        for (int i = 0; i < Q; i++) {
+
+            String command = in.next();
+
+            if (command.equals("GERAK")) {
+                gerak();
+            } else if (command.equals("HANCUR")) {
+                hancur();
+            } else if (command.equals("TAMBAH")) {
+                tambah();
+            } else if (command.equals("PINDAH")) {
+                pindah();
+            }
+        }
+
+        out.close();
+    }
+
+    // TODO: Implemen perintah GERAK
+    static void gerak() {
+
+    }
+
+    // TODO: Implemen perintah HANCUR
+    static void hancur() {
+
+    }
+
+    // TODO: Implemen perintah TAMBAH
+    static void tambah() {
+
+    }
+
+    // TODO: Implemen perintah PINDAH
+    static void pindah() {
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
+
+// lantai adalah sebuah node dalam doubly linkedlist gedung
+class Lantai {
+    boolean isDenjiExist; 
+    boolean isIblisExist;
+    Lantai prev; // refer ke index lantai sebelumnya
+    Lantai next; // refer ke index lantai selanjutnya 
+
+    Lantai (boolean isDenjiExist, boolean isIblisExist, Lantai prev, Lantai next) {
+        this.isDenjiExist = isDenjiExist;
+        this.isIblisExist = isIblisExist;
+        this.prev = prev;
+        this.next = next;
+    } 
+}
+
+class Gedung {
+    String nama; // nama gedung
+    Lantai first; // lantai dasar
+    Lantai last; // lantai teratas
+
+    Gedung (String nama, Lantai first, Lantai last) {
+        this.nama = nama;
+        this.first = first;
+        this.last = last;
+    }
+}
