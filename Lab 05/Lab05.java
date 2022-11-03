042d3f2e9f3c5559872c4a71eda4e54432f15447
eugeniusms <eugeniusms@gmail.com>
2022-11-03 21:57:27 +0700

    Add delete Node

--- Lab 05/Lab05.java
+++ Lab 05/Lab05.java
@@ -98,8 +98,8 @@ class AVLTree {
         y.left = T2;
   
         // Update tingginya
-        y.height = max(height(y.left), height(y.right)) + 1;
-        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = max(getHeight(x.left), getHeight(x.right)) + 1;
   
         // Return root baru
         return x;
@@ -114,28 +114,28 @@ class AVLTree {
         x.right = T2;
   
         // Update tingginya
-        x.height = max(height(x.left), height(x.right)) + 1;
-        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(getHeight(x.left), getHeight(x.right)) + 1;
+        y.height = max(getHeight(y.left), getHeight(y.right)) + 1;
   
         // Return root baru
         return y;
     }
 
-    Node insert(Node node, int key, String playerName) {
+    Node insertNode(Node node, int key, String playerName) {
         /* 1.  Perform the normal BST insertion */
         if (node == null)
             return (new Node(key, playerName));
   
         if (key < node.key)
-            node.left = insert(node.left, key, playerName);
+            node.left = insertNode(node.left, key, playerName);
         else if (key > node.key)
-            node.right = insert(node.right, key, playerName);
+            node.right = insertNode(node.right, key, playerName);
         else // Duplicate keys not allowed
             return node;
   
         /* 2. Update height of this ancestor node */
-        node.height = 1 + max(height(node.left),
-                              height(node.right));
+        node.height = 1 + max(getHeight(node.left),
+                              getHeight(node.right));
   
         /* 3. Get the balance factor of this ancestor
               node to check whether this node became
@@ -168,8 +168,94 @@ class AVLTree {
     }
 
     Node deleteNode(Node node, int key) {
-        // TODO: implement delete node
-        return null;
+        // STEP 1: PERFORM STANDARD BST DELETE 
+        if (root == null) 
+            return root; 
+  
+        // If the key to be deleted is smaller than 
+        // the root's key, then it lies in left subtree 
+        if (key < root.key) 
+            root.left = deleteNode(root.left, key); 
+  
+        // If the key to be deleted is greater than the 
+        // root's key, then it lies in right subtree 
+        else if (key > root.key) 
+            root.right = deleteNode(root.right, key); 
+  
+        // if key is same as root's key, then this is the node 
+        // to be deleted 
+        else
+        { 
+  
+            // node with only one child or no child 
+            if ((root.left == null) || (root.right == null)) 
+            { 
+                Node temp = null; 
+                if (temp == root.left) 
+                    temp = root.right; 
+                else
+                    temp = root.left; 
+  
+                // No child case 
+                if (temp == null) 
+                { 
+                    temp = root; 
+                    root = null; 
+                } 
+                else // One child case 
+                    root = temp; // Copy the contents of 
+                                // the non-empty child 
+            } 
+            else
+            { 
+  
+                // node with two children: Get the inorder 
+                // successor (smallest in the right subtree) 
+                Node temp = minValueNode(root.right); 
+  
+                // Copy the inorder successor's data to this node 
+                root.key = temp.key; 
+  
+                // Delete the inorder successor 
+                root.right = deleteNode(root.right, temp.key); 
+            } 
+        } 
+  
+        // If the tree had only one node then return 
+        if (root == null) 
+            return root; 
+  
+        // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE 
+        root.height = max(getHeight(root.left), getHeight(root.right)) + 1; 
+  
+        // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to check whether 
+        // this node became unbalanced) 
+        int balance = getBalance(root); 
+  
+        // If this node becomes unbalanced, then there are 4 cases 
+        // Left Left Case 
+        if (balance > 1 && getBalance(root.left) >= 0) 
+            return rightRotate(root); 
+  
+        // Left Right Case 
+        if (balance > 1 && getBalance(root.left) < 0) 
+        { 
+            root.left = leftRotate(root.left); 
+            return rightRotate(root); 
+        } 
+  
+        // Right Right Case 
+        if (balance < -1 && getBalance(root.right) <= 0) 
+            return leftRotate(root); 
+  
+        // Right Left Case 
+        if (balance < -1 && getBalance(root.right) > 0) 
+        { 
+            root.right = rightRotate(root.right); 
+            return leftRotate(root); 
+        } 
+  
+        return root; 
     }
 
     Node lowerBound(Node node, int value) {
@@ -183,7 +269,7 @@ class AVLTree {
     }
 
     // Utility function to get height of node
-    int height(Node node) {
+    int getHeight(Node node) {
         if (node == null) {
             return 0;
         }
@@ -195,7 +281,7 @@ class AVLTree {
         if (node == null) {
             return 0;
         }
-        return height(node.left) - height(node.right);
+        return getHeight(node.left) - getHeight(node.right);
     }
 
     // Utility function to get maximum of two integers
@@ -206,3 +292,4 @@ class AVLTree {
 
 // REFERENCES:
 // 1) https://www.geeksforgeeks.org/insertion-in-an-avl-tree/
+// 2) https://www.geeksforgeeks.org/deletion-in-an-avl-tree/
