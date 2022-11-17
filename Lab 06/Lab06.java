93a50dfc539bd96965e559ff005406b14e18a1c5
eugeniusms <eugeniusms@gmail.com>
2022-11-18 01:15:40 +0700

    Add MaxHeap

--- Lab 06/Lab06.java
+++ Lab 06/Lab06.java
@@ -92,5 +92,108 @@ class Heap {
     }
 }
 
+class MaxHeap {
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+ 
+    // Constructor to initialize an
+    // empty max heap with given maximum
+    // capacity
+    MaxHeap(int maxsize) {
+        // This keyword refers to current instance itself
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize];
+    }
+ 
+    // Returning position of parent
+    private int parent(int pos) { return (pos - 1) / 2; }
+ 
+    // Returning left children
+    private int leftChild(int pos) { return (2 * pos) + 1; }
+
+    // Returning right children
+    private int rightChild(int pos) { return (2 * pos) + 2; }
+ 
+    // Returning true of given node is leaf
+    private boolean isLeaf(int pos) { return (pos >= (size / 2) && pos <= size); }
+ 
+    // Swapping nodes
+    private void swap(int fpos, int spos) {
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+ 
+    // Recursive function to max heapify given subtree
+    private void maxHeapify(int pos) {
+        if (isLeaf(pos))
+            return;
+ 
+        if (Heap[pos] < Heap[leftChild(pos)]
+            || Heap[pos] < Heap[rightChild(pos)]) {
+ 
+            if (Heap[leftChild(pos)]
+                > Heap[rightChild(pos)]) {
+                swap(pos, leftChild(pos));
+                maxHeapify(leftChild(pos));
+            }
+            else {
+                swap(pos, rightChild(pos));
+                maxHeapify(rightChild(pos));
+            }
+        }
+    }
+ 
+    // Inserts a new element to max heap
+    public void insert(int element) {
+        Heap[size] = element;
+ 
+        // Traverse up and fix violated property
+        int current = size;
+        while (Heap[current] > Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+        size++;
+    }
+ 
+    // To display heap
+    public void print() {
+ 
+        for (int i = 0; i < size / 2; i++) {
+ 
+            System.out.print("Parent Node : " + Heap[i]);
+ 
+            if (leftChild(i)
+                < size) // if the child is out of the bound
+                        // of the array
+                System.out.print(" Left Child Node: "
+                                 + Heap[leftChild(i)]);
+ 
+            if (rightChild(i)
+                < size) // if the right child index must not
+                        // be out of the index of the array
+                System.out.print(" Right Child Node: "
+                                 + Heap[rightChild(i)]);
+ 
+            System.out.println(); // for new line
+        }
+    }
+
+    // Remove an element from max heap
+    public int extractMax() {
+        int popped = Heap[0];
+        Heap[0] = Heap[--size];
+        maxHeapify(0);
+        return popped;
+    }
+}
+
+
 // MARIO'S REFERENCES:
 // 1) https://stackoverflow.com/questions/15319561/how-to-implement-a-median-heap/15319593#15319593
+// 2) https://www.geeksforgeeks.org/max-heap-in-java/
+// 3) 
