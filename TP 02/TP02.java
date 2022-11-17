7294db9f0c8870d565ff75261417654e8cb327ba
eugeniusms <eugeniusms@gmail.com>
2022-11-17 16:22:37 +0700

    Add Kasus saat popularity == 0, harusnya saat dihapus walaupun udah gaada tapi budi pindah(?)

--- TP 02/TP02.java
+++ TP 02/TP02.java
@@ -53,8 +53,8 @@ public class TP02 {
             } else if (query.equals("EVALUASI")) {
                 EVALUASI();
             }
-            // daftarMesin.print();
-            // daftarMesin.budiNow.scoreTree.printInOrder();
+            daftarMesin.print();
+            daftarMesin.budiNow.scoreTree.printInOrder();
         }
 
         out.close();
@@ -95,6 +95,7 @@ public class TP02 {
         if(daftarMesin.budiNow.popularity <= X) {
             if (daftarMesin.budiNow.popularity <= 0) {
                 out.println("0");
+                daftarMesin.pindahMesin(daftarMesin.budiNow);  
             } else {
                 out.println(daftarMesin.budiNow.scoreTree.root.sum);
                 daftarMesin.budiNow.scoreTree = new AVLTree();; // set to null reset AVLTree
