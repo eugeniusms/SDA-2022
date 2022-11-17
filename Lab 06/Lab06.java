f9cbc0f582906b8dbd33b3b200ff2068a3e2bf89
eugeniusms <eugeniusms@gmail.com>
2022-11-18 01:48:26 +0700

    berhasil sorting array

--- Lab 06/Lab06.java
+++ Lab 06/Lab06.java
@@ -27,88 +27,31 @@ public class Lab06 {
             initialSaham[i-1] = saham;
         }
 
-        int Q = in.nextInt();
+        // sort saham
+        Arrays.sort(initialSaham);
 
-        // TODO
-        for (int i = 0; i < Q; i++) {
-            String q = in.next();
-
-            if (q.equals("TAMBAH")) {
-                int harga = in.nextInt();
-
-            } else if (q.equals("UBAH")) {
-                int nomorSeri = in.nextInt();
-                int harga = in.nextInt();
-
-            }
+        // cek isi array
+        for(int i = 0; i < N; i++) {
+            System.out.println("[HARGA: "+initialSaham[i].harga + " ID: " + initialSaham[i].id+"]");
         }
+
+        // int Q = in.nextInt();
+
+        // // TODO
+        // for (int i = 0; i < Q; i++) {
+        //     String q = in.next();
+
+        //     if (q.equals("TAMBAH")) {
+        //         int harga = in.nextInt();
+
+        //     } else if (q.equals("UBAH")) {
+        //         int nomorSeri = in.nextInt();
+        //         int harga = in.nextInt();
+
+        //     }
+        // }
         out.flush();
     }
-
-    // Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
-    void merge(Saham arr[], int l, int m, int r) {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
- 
-        /* Create temp arrays */
-        Saham L[] = new Saham[n1];
-        Saham R[] = new Saham[n2];
- 
-        /*Copy data to temp arrays*/
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
- 
-        /* Merge the temp arrays */
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
- 
-        // Initial index of merged subarray array
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i].isLessThan(R[j])) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
- 
-        /* Copy remaining elements of L[] if any */
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-        /* Copy remaining elements of R[] if any */
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
- 
-    // Main function that sorts arr[l..r] using
-    // merge()
-    void sort(Saham arr[], int l, int r) {
-        if (l < r) {
-            // Find the middle point
-            int m = l + (r - l) / 2;
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
-            // Merge the sorted halves
-            merge(arr, l, m, r);
-        }
-    }
- 
 
     static class InputReader {
         public BufferedReader reader;
