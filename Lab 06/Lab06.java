a62b738bd301cd6aa685e038f64f86ff443efe1b
eugeniusms <eugeniusms@gmail.com>
2022-11-18 19:30:21 +0700

    remove maxHeapify() from method

--- Lab 06/Lab06.java
+++ Lab 06/Lab06.java
@@ -359,25 +359,6 @@ class MinHeap {
         Heap[fpos] = Heap[spos];
         Heap[spos] = tmp;
     }
-
-    // Recursive function to max heapify given subtree
-    private void maxHeapify(int pos) {
-        if (isLeaf(pos))
-            return;
- 
-        if (Heap[pos].isLessThan(Heap[leftChild(pos)])
-            || Heap[pos].isLessThan(Heap[rightChild(pos)])) {
- 
-            if (Heap[rightChild(pos)].isLessThan(Heap[leftChild(pos)])) {
-                swap(pos, leftChild(pos));
-                maxHeapify(leftChild(pos));
-            }
-            else {
-                swap(pos, rightChild(pos));
-                maxHeapify(rightChild(pos));
-            }
-        }
-    }
  
     // Recursive function to min heapify given subtree
     private void minHeapify(int pos) {
