e0b50b883ffd62579ad223c10c0563dc23e7a800
eugeniusms <eugeniusms@gmail.com>
2022-11-24 20:47:02 +0700

    add in out

--- Lab 07/Lab07Dijkstra.java
+++ Lab 07/Lab07Dijkstra.java
@@ -3,14 +3,18 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.*;
 import java.util.*;
 import java.util.StringTokenizer;
 // Java Program to Implement Dijkstra's Algorithm
 // Using Priority Queue
-
  
 // Main class DPQ
 public class Lab07Dijkstra {
+    private static InputReader in;
+    private static PrintWriter out;
  
     // Member variables of this class
     private int dist[];
