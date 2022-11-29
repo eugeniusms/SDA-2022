f6fa617460442aa6e11ee5053adfdb3aeb0eb7fc
eugeniusms <eugeniusms@gmail.com>
2022-11-29 09:14:18 +0700

    🔧 Add Kruskal's Template

---
+++ Graph/Kruskal/Kruskal.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Kruskal {
+    private static InputReader in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        in = new InputReader(inputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
+
+        // Tutup OutputStream
+        out.close();
+    }   
+
+    // taken from https://codeforces.com/submissions/Petr
+    // together with PrintWriter, these input-output (IO) is much faster than the
+    // usual Scanner(System.in) and System.out
+    // please use these classes to avoid your fast algorithm gets Time Limit
+    // Exceeded caused by slow input-output (IO)
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public char nextChar() {
+            return next().equals("R") ? 'R' : 'B';
+        }
+    }
+}
+
+class Edge {
+    int start;
+    int destination;
+    long cost;
+
+    Edge(int start, int destination, long cost) {
+        this.start = start;
+        this.destination = destination;
+        this.cost = cost;
+    }
+}
