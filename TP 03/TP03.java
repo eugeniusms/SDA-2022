427de58c90e4b6574533bd5eb156d6ac1a3b5c16
eugeniusms <eugeniusms@gmail.com>
2022-11-30 10:03:18 +0700

    [SUBMITTED01] - QUERY SIMULASI

--- TP 03/TP03.java
+++ TP 03/TP03.java
@@ -16,10 +16,6 @@ public class TP03 {
     static long dist[];
     static List<Integer> settled;
     static MinHeap<Node> mh;
-
-    static long distMax[];
-    static List<Integer> settledMax;
-    static MaxHeap maxH;
     // Number of vertices
     static int V;
     static List<List<Node> > adj;
@@ -155,51 +151,9 @@ public class TP03 {
         }
     }
 
-    // DijkstraMax
-    static void inisiateDijkstraMax(int v) {
-        V = v;
-        distMax = new long[v];
-        settledMax = new ArrayList<Integer>();
-        maxH = new MaxHeap();
-    }
-
-    static void dijkstraMax(int src) {
-        for (int i = 0; i < V; i++)
-            distMax[i] = Long.MIN_VALUE;
-        maxH.insert(new Node(src, 0, 0));
-        distMax[src] = 0;
-        while (settledMax.size() != V) {
-            if (maxH.isEmpty())
-                return;
-            int u = maxH.remove().node;
-            if (settledMax.contains(u))
-                continue;
-            settledMax.add(u);
-            e_NeighboursMax(u);
-        }
-    }
-
-    static void e_NeighboursMax(int u) {
-        long edgeDistance = -1;
-        long newDistance = -1;
-        for (int i = 0; i < adj.get(u).size(); i++) {
-            Node v = adj.get(u).get(i);
-            if (!settledMax.contains(v.node)) {
-                edgeDistance = v.S;
-                newDistance = distMax[u] + edgeDistance;
-                if (newDistance > distMax[v.node])
-                    distMax[v.node] = newDistance;
-                maxH.insert(new Node(v.node, distMax[v.node], v.S));
-            }
-        }
-    }
-
     // QUERY 1 : KABUR
     static void KABUR(int VE) {
         int source = in.nextInt(); int destination = in.nextInt();
-        // inisiateDijkstraMax(VE); // reset
-        // dijkstraMax(source);
-        // out.println(distMax[destination]);
     }
 
     // QUERY 2 : SIMULASI
@@ -307,14 +261,6 @@ class Node implements Comparable<Node> {
         if (this.L < other.L)
             return -1;
         if (this.L > other.L)
-            return 1;
-        return 0;
-    }
-
-    public int compareTo2(Node other) {
-        if (this.S < other.S) 
-            return -1;
-        if (this.S > this.S) 
             return 1;
         return 0;
     }
@@ -474,149 +420,6 @@ class MinHeap<T extends Comparable<T>> {
 					minChildIdx = rightIdx;
 
 				if (node.compareTo(data.get(minChildIdx)) > 0) {
-					data.set(idx, data.get(minChildIdx));
-					idx = minChildIdx;
-				} else {
-					data.set(idx, node);
-					break;
-				}
-			}
-		}
-	}
-
-}
-
-class MaxHeap {
-	ArrayList<Node> data;
-
-	public MaxHeap() {
-		data = new ArrayList<Node>();
-	}
-
-	public MaxHeap(ArrayList<Node> arr) {
-		data = arr;
-		heapify();
-	}
-
-    public boolean isEmpty() {
-        return data.isEmpty();
-    }
-
-	public Node peek() {
-		if (data.isEmpty())
-			return null;
-		return data.get(0);
-	}
-
-	public void insert(Node value) {
-		data.add(value);
-		percolateUp(data.size() - 1);
-	}
-
-	public Node remove() {
-		Node removedObject = peek();
-
-		if (data.size() == 1)
-			data.clear();
-		else {
-			data.set(0, data.get(data.size() - 1));
-			data.remove(data.size() - 1);
-			percolateDown(0);
-		}
-
-		return removedObject;
-	}
-
-	private void percolateDown(int idx) {
-		Node node = data.get(idx);
-		int heapSize = data.size();
-
-		while (true) {
-			int leftIdx = getLeftChildIdx(idx);
-			if (leftIdx >= heapSize) {
-				data.set(idx, node);
-				break;
-			} else {
-				int minChildIdx = leftIdx;
-				int rightIdx = getRightChildIdx(idx);
-				if (rightIdx < heapSize && data.get(rightIdx).compareTo2(data.get(leftIdx)) > 0)
-					minChildIdx = rightIdx;
-
-				if (node.compareTo2(data.get(minChildIdx)) < 0) {
-					data.set(idx, data.get(minChildIdx));
-					idx = minChildIdx;
-				} else {
-					data.set(idx, node);
-					break;
-				}
-			}
-		}
-	}
-
-	private void percolateUp(int idx) {
-		Node node = data.get(idx);
-		int parentIdx = getParentIdx(idx);
-		while (idx > 0 && node.compareTo2(data.get(parentIdx)) > 0) {
-			data.set(idx, data.get(parentIdx));
-			idx = parentIdx;
-			parentIdx = getParentIdx(idx);
-		}
-
-		data.set(idx, node);
-	}
-
-	private int getParentIdx(int i) {
-		return (i - 1) / 2;
-	}
-
-	private int getLeftChildIdx(int i) {
-		return 2 * i + 1;
-	}
-
-	private int getRightChildIdx(int i) {
-		return 2 * i + 2;
-	}
-
-	private void heapify() {
-		for (int i = data.size() / 2 - 1; i >= 0; i--)
-			percolateDown(i);
-	}
-
-	public void sort() {
-		int n = data.size();
-		while (n > 1) {
-			data.set(n - 1, remove(n));
-			n--;
-		}
-	}
-
-	public Node remove(int n) {
-		Node removedObject = peek();
-
-		if (n > 1) {
-			data.set(0, data.get(n - 1));
-			percolateDown(0, n - 1);
-		}
-
-		return removedObject;
-	}
-
-	private void percolateDown(int idx, int n) {
-		Node node = data.get(idx);
-		int heapSize = n;
-
-		while (true) {
-			int leftIdx = getLeftChildIdx(idx);
-			if (leftIdx >= heapSize) {
-				data.set(idx, node);
-				break;
-			} else {
-				int minChildIdx = leftIdx;
-				int rightIdx = getRightChildIdx(idx);
-				if (rightIdx < heapSize && data.get(rightIdx).compareTo2(data.get(leftIdx)) > 0)
-					minChildIdx = rightIdx;
-
-				if (node.compareTo(data.get(minChildIdx)) < 0) {
 					data.set(idx, data.get(minChildIdx));
 					idx = minChildIdx;
 				} else {
