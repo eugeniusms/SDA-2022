e3492e4f81883df05fcdedac28fd5a7a52cf7b2d
eugeniusms <eugeniusms@gmail.com>
2023-02-10 19:57:04 +0700

    🛠 Restructured Files

--- Checkpoint 01/Question6.java
+++
@@ -1,9 +0,0 @@
-public class Question6 {
-    public static void main(String[] args) {
-        int N = 100;
-        for(int i = 100; i < N*N; i+=2)
-            System.out.println("print something");
-        for(int j = 1; j < N; j*=2)
-            System.out.println("print anything");
-    }
-}
--- TP 01 - SDA E/TP01.py
+++
@@ -1,7 +0,0 @@
-penyihir = ["Tara", "Kenneth"]
-
-penyihir_duluan = input()
-cawan_1 = int(input())
-cawan_2 = int(input())
-
-
